636842a90c158bce11bcb2421154ef3e6b5422f6
rahul007-bit <yadavrahul8141@yahoo.com>
2026-06-21 16:58:35 +0530

    clean: remove debug logging from dnf.py

--- syncit/plugins/dnf.py
+++ syncit/plugins/dnf.py
@@ -235,11 +235,7 @@ class DnfPlugin(OfflinePlugin):
             dl_cmd.extend(extra_repo_opts)
             dl_cmd.extend(packages)
 
-            if ctx.verbose:
-                err_console.print(f"[DEBUG DNF] task_spec.get('arch') = {task_spec.get('arch')}")
-                err_console.print(f"[DEBUG DNF] ctx.targets = {ctx.targets}")
-                err_console.print(f"[DEBUG DNF] resolved arch = {arch}")
-                err_console.print(f"[DEBUG DNF] dl_cmd = {dl_cmd}")
+
 
             res = subprocess.run(dl_cmd, capture_output=True, text=True)
             if res.returncode != 0:
